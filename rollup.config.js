ef8c85c6262f5834bd6956f39f70296108ab1eda
sacru2red <sacru2red@gmail.com>
2022-07-20 20:35:56 +0900

    convert to typescript

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import commonjs from "@rollup/plugin-commonjs"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import typescript from '@rollup/plugin-typescript';
+import json from "@rollup/plugin-json";
+
+const inputSrc = [
+  ["./src/index.ts", "es"],
+  ["./src/index.ts", "cjs"],
+]
+
+const extensions = [".mjs", ".json", ".ts", ".native.ts", ".native.js"]
+
+export default inputSrc
+  .map(([input, format]) => {
+    return {
+      input,
+      output: {
+        dir: `lib/${format}`,
+        format,
+      },
+      preserveModules: true,
+      external: [
+        "@react-native-module/randombytes",
+        "browserify-cipher",
+        "browserify-sign",
+        'browserify-sign/browser/algorithms.json',
+        "create-ecdh",
+        "create-hash",
+        "create-hmac",
+        "diffie-hellman",
+        "elliptic",
+        "inherits",
+        "pbkdf2",
+        "public-encrypt",
+      ],
+      plugins: [
+        json(),
+        typescript(),
+        nodeResolve(),
+        // https://velog.io/@peterkimzz/rollup-%ED%94%8C%EB%9F%AC%EA%B7%B8%EC%9D%B8
+        // CommonJS 로 작성된 모듈들을 ES6 바꾸어서 rollup이 해석할 수 있게 도와줍니다.
+        commonjs({
+          extensions: [...extensions, ".js"],
+        }),
+      ]
+    }
+  })
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import createHashModule from 'create-hash'
+import createHmacModule from 'create-hmac'
+import randomBytesModule from '@react-native-module/randombytes'
+import signAlogos from 'browserify-sign/browser/algorithms.json'
+import pbkdf2Module from 'pbkdf2'
+import aes from 'browserify-cipher'
+import dh from 'diffie-hellman'
+import sign from 'browserify-sign'
+import createEcdhModule from 'create-ecdh'
+import publicEncryptModule from 'public-encrypt'
+
+export const Hash = createHashModule
+export const createHash = Hash
+export const Hmac = createHmacModule
+export const createHmac = createHmacModule
+export const randomBytes = randomBytesModule.randomBytes
+export const rng = randomBytes
+export const pseudoRandomBytes = randomBytes
+export const prng = randomBytes
+
+const hashes = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md5', 'rmd160'].concat(Object.keys(signAlogos))
+export function getHashes (): string[] {
+  return hashes
+}
+export const pbkdf2 = pbkdf2Module.pbkdf2
+export const pbkdf2Sync = pbkdf2Module.pbkdf2Sync
+
+export const Cipher = aes.Cipher
+export const createCipher = aes.createCipher
+export const Cipheriv = aes.Cipheriv
+export const createCipheriv = aes.createCipheriv
+export const Decipher = aes.Decipher
+export const createDecipher = aes.createDecipher
+export const Decipheriv = aes.Decipheriv
+export const createDecipheriv = aes.createDecipheriv
+export const getCiphers = aes.getCiphers
+export const listCiphers = aes.listCiphers
+
+export const DiffieHellmanGroup = dh.DiffieHellmanGroup
+export const createDiffieHellmanGroup = dh.createDiffieHellmanGroup
+export const getDiffieHellman = dh.getDiffieHellman
+export const createDiffieHellman = dh.createDiffieHellman
+export const DiffieHellman = dh.DiffieHellman
+
+export const createSign = sign.createSign
+export const Sign = sign.Sign
+export const createVerify = sign.createVerify
+export const Verify = sign.Verify
+
+export const createECDH = createEcdhModule
+
+export const publicEncrypt = publicEncryptModule.publicEncrypt
+export const privateEncrypt = publicEncryptModule.privateEncrypt
+export const publicDecrypt = publicEncryptModule.publicDecrypt
+export const privateDecrypt = publicEncryptModule.privateDecrypt
+
+export const createCredentials = (): any => {
+  throw new Error([
+    'sorry, createCredentials is not implemented yet',
+    'we accept pull requests',
+    'https://github.com/crypto-browserify/crypto-browserify'
+  ].join('\n'))
+}
